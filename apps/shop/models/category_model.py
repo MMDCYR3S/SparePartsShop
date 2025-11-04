b5b5cfd2b36bc6824ea5876e68be14b5bd52ead9
CYR3S <amingholami06@gmail.com>
2025-11-04 19:55:38 +0330

    ایجاد اسلاگ برای دسته‌بندی قطعه

--- apps/shop/models/category_model.py
+++ apps/shop/models/category_model.py
@@ -1,4 +1,5 @@
 from django.db import models
+from slugify import slugify
 
 # ======= Category Model ======= #
 class Category(models.Model):
@@ -6,11 +7,21 @@ class Category(models.Model):
     مدل برای دسته‌بندی‌ها و زیردسته‌بندی‌ها
     """
     name = models.CharField(max_length=100, verbose_name="نام دسته‌بندی")
+    slug = models.SlugField(max_length=100, unique=True, null=True, blank=True, verbose_name="اسلاگ")
     parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children', verbose_name="دسته‌بندی والد")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="تاریخ ایجاد")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="تاریخ بروزرسانی")
     
     def __str__(self):
         return self.name
 
+    def save(self):
+        """ ذخیره اسلاگ به صورت انگلیسی """
+        
+        self.slug = slugify(self.name)
+        super().save()
+        
+    
     class Meta:
         verbose_name = "دسته‌بندی"
         verbose_name_plural = "دسته‌بندی‌ها"
